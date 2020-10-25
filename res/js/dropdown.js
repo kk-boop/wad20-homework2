2d30b6fc44274259bfeb4edf0f436ae127ca6971
kk-boop <56920559+kk-boop@users.noreply.github.com>
2020-10-25 20:20:21 +0200

    initial dropdown

---
+++ res/js/dropdown.js
@@ -0,0 +1,7 @@
+const toggleDropdown = function() {
+    $(".dropdown-content").addClass("dropdown-visible");
+    console.log("bss")
+};
+
+$(".dropdown").click(toggleDropdown);
+$(".dropdown").click(toggleDropdown);
